2c346cc06dc7b1948fdbb2f4beed559f3b6d49f1
Fahiye Muhammad <fahiye254@gmail.com>
2025-04-16 19:14:32 +0300

    Learning use effect in react

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
 
   // add useEffect code
 
+
   function handleAnswer(isCorrect) {
-    setTimeRemaining(10);
+    setTimeRemaining(timeRemaining);
     onAnswered(isCorrect);
   }
 
+  
+  useEffect(() => {
+    let timeoutId;
+  
+    function tick() {
+      setTimeRemaining(prevTime => {
+        if (prevTime <= 1) {
+          onAnswered(false); // Time's up
+          return 0;
+        } else {
+          timeoutId = setTimeout(tick, 1000);
+          return prevTime - 1;
+        }
+      });
+    }
+  
+    timeoutId = setTimeout(tick, 1000);
+  
+    return () => clearTimeout(timeoutId); // cleanup on unmount
+  }, []);
   const { id, prompt, answers, correctIndex } = question;
 
   return (
